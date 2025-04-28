5af33f94a526cbe13115d83d847965cf7d7b5f45
AizahAkbar <aizahakbar687@gmail.com>
2025-04-28 21:37:06 +0100

    reviews implemented (#9)

---
+++ FinalYearProject/Controllers/ReviewController.cs
@@ -0,0 +1,55 @@
+﻿using FinalYearProject.Services;
+using FinalYearProject.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Stripe;
+
+namespace FinalYearProject.Controllers
+{
+    public class ReviewController : Controller
+    {
+        private readonly IReviewService _reviewService;
+
+        public ReviewController(IReviewService reviewService)
+        {
+            _reviewService = reviewService;
+        }
+
+        public async Task<IActionResult> GetReviews(int bakeId)
+        {
+            var (averageRating, reviewCount) = await _reviewService.GetAverageRatingAsync(bakeId);
+            return Json(new
+            {
+                averageRating = averageRating,
+                reviewCount = reviewCount
+            });
+        }
+
+        public IActionResult Create(int bakeId)
+        {
+            var userIdStr = HttpContext.Session.GetInt32("UserId");
+            if (!userIdStr.HasValue)
+            {
+                return RedirectToAction("Login", "User");
+            }
+
+            var model = new ReviewViewModel { BakeId = bakeId };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ReviewViewModel model)
+        {
+            var userIdStr = HttpContext.Session.GetInt32("UserId");
+            if (!userIdStr.HasValue)
+            {
+                return RedirectToAction("Login", "User");
+            }
+
+            model.UserId = userIdStr.Value.ToString();
+
+            await _reviewService.AddReviewAsync(model);
+
+            return RedirectToAction("Details", "Bakes", new { id = model.BakeId });
+        }
+    }
+}
---
+++ FinalYearProject/Services/IReviewService.cs
@@ -0,0 +1,10 @@
+﻿using FinalYearProject.ViewModels;
+
+namespace FinalYearProject.Services
+{
+    public interface IReviewService
+    {
+        Task AddReviewAsync(ReviewViewModel model);
+        Task<(double AverageRating, int ReviewCount)> GetAverageRatingAsync(int bakeId);
+    }
+}
